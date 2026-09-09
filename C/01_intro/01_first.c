670e920e645b92358d6a4467eb95654cc46f554a
meetsheladiya07 <meetsheladiya567@gmail.com>
2026-09-09 22:26:26 +0530

    c

---
+++ C/01_intro/01_first.c
@@ -0,0 +1,10 @@
+#include<stdio.h> // stadread input output herder file
+#include<conio.h> // console log input output heder file 
+
+// inbuilt function 
+//single comment
+int main()
+{
+	printf("Hello This Is First C Program ");
+	return 0;
+}
---
+++ C/01_intro/03_student_list.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	/* mutli line comment
+	printf("----Student List ----");
+	printf("\n1.Meet");
+	printf("\n2.Ridham");
+	printf("\n3.Monil");
+	printf("\n4.Aakash");
+	*/
+	
+	printf("----Student List ----\n1.Meet\n2.Ridham\n3.Monil\n4.Aakash");
+	
+	getch();
+}
